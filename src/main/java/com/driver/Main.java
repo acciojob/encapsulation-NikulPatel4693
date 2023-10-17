1b8fc05ce847fa3062b432164ea6811c5b830ec2
NikulPatel4693 <121799515+NikulPatel4693@users.noreply.github.com>
2023-10-17 22:06:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,10 @@
 package com.driver;
 
 public class Main {
+  public static void main(String[] args){
   RWOnly obj = new RWOnly();
   obj.setName("Nikul");
   String name = obj.getName();
   System.out.println(name);
+  }
 }
